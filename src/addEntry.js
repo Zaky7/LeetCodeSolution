c871dea9eff673f231f3c6543c1c80ec2455cbd4
Zaky7 <zak.saifi7@gmail.com>
2020-05-10 18:59:50 +0530

    feat: automatically adding entry feat added

--- src/addEntry.js
+++ src/addEntry.js
@@ -1,23 +1,68 @@
 const fs = require("fs");
+const mkdirp = require("mkdirp");
+const inquirer = require("inquirer");
+const chalk = require("chalk");
 
 const README_PATH = "./README.md";
 const ENCODING = "utf8";
 
-const inquirer = require("inquirer");
 
-function getCamelCaseFileName(str) {
+function getCamelCase(str) {
   return str
     .split(" ")
     .map((ele) => ele && ele[0].toUpperCase() + ele.substring(1, ele.length))
     .join("");
 }
 
-function generateReadmeTemplate(prob) {
-  const { name, code, url } = prob;
-  const fileName = getCamelCaseFileName(name);
-  const template = `|${code}|[${name}](${url})|[[Java]](./src/array/${fileName}.java)|Medium|[[Docs]](./src/array/${fileName}.md)`;
-  console.log(template);
+function generateReadmeTemplate(prob, codefilePath, docfilePath) {
+  const { name, code, url, difficulty } = prob;
+  const template = `| # | Title | Solution | Difficulty | Docs |\n|---| ----- | -------- | ---------- | ---- |\n|${code}|[${name}](${url})|[[Java]](${codefilePath})|${difficulty}|[[Docs]](${docfilePath})\n\n\n`;
   return template;
+}
+
+function appendToReadMe(data) {
+  fs.appendFileSync(README_PATH, data, 'utf8');
+}
+
+function createDir(dirpath) {
+  mkdirp.sync(dirpath);
+  // console.log(`made directories, starting with ${dirpath}`);
+}
+
+function createCodeDir(dirname) {
+  const dirpath = "./src/" + dirname;
+  createDir(dirpath);
+  return dirpath;
+}
+
+function createDocDir(dirname) {
+  const dirpath = "./src/" + dirname + "Doc";
+  createDir(dirpath);
+  return dirpath;
+}
+
+function addEntry(prob) {
+  const fileName    = getCamelCase(prob.name);
+  const codeDirPath = createCodeDir(prob.type);
+  const docDirPath  = createDocDir(prob.type);
+  
+  const codefilePath =  createFile(codeDirPath, fileName, ".java");
+  const docfilePath  =  createFile(docDirPath, fileName, ".md");
+  const template = generateReadmeTemplate(prob, codefilePath, docfilePath);
+  appendToReadMe(template);
+
+  console.log(chalk.green.bold("Changes saved :) "))
+}
+
+function createFile(dirPath, fileName, extension) {
+  const filePath = dirPath + "/" + fileName + extension;
+  // console.log(filePath);
+  if(!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, "", "utf8");
+  } else {
+    console.log(chalk.red.bold("File Exists"));
+  }
+  return filePath;
 }
 
 inquirer
@@ -36,8 +81,15 @@ inquirer
       name: "problemType",
       type: "list",
       message: "Which type of problem it is?",
-      choices: ["Array", "Stack", "LinkedList", "Recursion && Dp"],
+      choices: ["array", "stack", "linkedList", "recursionOrDp"],
       default: 4,
+    },
+    {
+      name: "problemDifficulty",
+      type: "list",
+      message: "What is the difficulty level of problem?",
+      choices: ["Easy", "Medium", "Hard"],
+      default: 3,
     },
     {
       name: "problemCode",
@@ -46,11 +98,20 @@ inquirer
     },
   ])
   .then((answers) => {
-    generateReadmeTemplate({
+    addEntry({
       code: answers.problemCode,
       name: answers.problemName,
       url: answers.problemUrl,
       type: answers.problemType,
+      difficulty: answers.problemDifficulty
     });
   })
-  .catch((err) => console.error(err));
+  .catch((err) => console.log(chalk.red.bold(err)));
+
+
+//  addEntry({
+//    code: 134,
+//    name: "Jump Game",
+//    url: "https://leetcode.com/problems/jump-game/",
+//    type: "recursionOrDp",
+//  });
